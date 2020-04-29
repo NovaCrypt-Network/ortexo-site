3bd3099afd7c6de806cc626a1c891236fd0bdc3e
Calix Huang <calix.huang1@gmail.com>
2020-04-29 15:21:01 -0700

    added logos and fixed alignment

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,8 @@
 var mySwiper = new Swiper ('.partners-container', {
   direction: 'horizontal',
-  slidesPerView: 5,
+  slidesPerView: 8,
   grabCursor: true,
+  autoplay: {
+    delay: 1500
+  },
 })
