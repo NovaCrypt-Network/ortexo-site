a068afe6c4c41bcfeba66df8d3c8eff903188467
Calix Huang <calix.huang1@gmail.com>
2020-05-03 09:48:05 -0700

    added more events

--- js/calendar.js
+++ js/calendar.js
@@ -22,20 +22,43 @@ document.addEventListener('DOMContentLoaded', function() {
         color: eventColor
       },
       {
-        title: 'Build and launch our HS organization cohort!',
+        title: 'Build and launch our HS organization hub!',
         start: '2020-05-03',
-        end: '2020-05-05',
+        end: '2020-05-07',
+        color: eventColor
+      },
+      {
+        title: 'Revamp and improve on the w3Hacks platform',
+        start: '2020-05-03',
+        end: '2020-05-07',
         color: eventColor
       },
       {
         title: 'Applications close!',
-        start: '2020-05-06',
+        start: '2020-05-06T21:00:00',
+        color: eventColor
+      },
+      {
+        title: 'First-round applications are analyzed',
+        start: '2020-05-07',
+        end: '2020-05-15',
         color: eventColor
       },
       {
         title: 'Second-round interview results are revealed!',
         start: '2020-05-15',
         end: '2020-05-17',
+        color: eventColor
+      },
+      {
+        title: 'Second-round interviews',
+        start: '2020-05-18',
+        end: '2020-05-23',
+        color: eventColor
+      },
+      {
+        title: 'Final results released!',
+        start: '2020-05-30',
         color: eventColor
       }
     ]
