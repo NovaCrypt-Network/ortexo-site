8407f9606e225b34055994f72702f91eb6033c0b
Calix Huang <calix.huang1@gmail.com>
2020-06-19 22:51:20 -0700

    added images, scss files, and index page + cdn links

--- pages/index.js
+++ pages/index.js
@@ -2,208 +2,130 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Index</title>
       </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <section className="header" id="header">
+        <div className="container">
+          <div className="header-grid">
+            <div className="header-grid-left" data-aos="fade-right">
+              <h1>Launch Tech LLC</h1>
+              <h4 className="header-grid-left-sub-header">
+                Launch Tech is a SaaS product holding company, dedicated to launching real-world software applications.
+              </h4>
+              <div className="cta-buttons mt-4">
+                <button className="button ghost-cta-button-light">Learn More</button>
+                <a href="/products" className="button solid-cta-button-light">Check out our products!</a>
+              </div>
+            </div>
+            <div className="header-grid-right" data-aos="zoom-in">
+              <img src="img/illustrations/header.png" alt="Launch Tech LLC header illustration - floating building" className="header-grid-right-illustration" />
+            </div>
+          </div>
         </div>
-      </main>
+      </section>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <div className="waves">
+        <img src="img/waves/header-wave.png" className="flipped-wave wave" />
+      </div>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+      <section className="about" id="about">
+        <div className="section-container container">
+          <div className="about-grid">
+            <div className="about-grid-left" data-aos="zoom-in">
+              <img src="img/illustrations/about.png" alt="Launch Tech LLC digital marketing illustration" className="about-grid-left-illustration" />
+            </div>
+            <div className="about-grid-right">
+              <h1 className="section-header">About Launch Tech</h1>
+              <p>
+                Launch Tech LLC is a SaaS (Software as a Service) product holding company, dedicated to launching real-world software applications. We create products that are useful, meaningful, and applicable to different people and different situations around the world, and we partner with other organizations to make this into a reality!
+                <br/><br/>
+                Though we are a SaaS product company, we try to open with our processes, builds, and deadlines. We have a calendar that shares all of the things we're working on, and when certain events/product-builds will start, end, and be deployed.
+              </p>
+              <div className="cta-buttons mt-4">
+                <a className="button ghost-cta-button-dark" href="/opportunities">Job Opportunities</a>
+                <a href="/products" className="button solid-cta-button-dark">Our Products</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+      <section className="featured-products-section" id="featured-products">
+        <div className="section-container container" data-aos="zoom-in">
+          <h1 className="section-header">Featured Products</h1>
+          <div className="products-grid mt-5">
+            <a className="product-link" href="https://npocore.com" target="_blank">
+              <img className="product-image" src="img/products/npocore/desktop.png" alt="Launch Tech LLC - NPO Core - Website Image" />
+              <div className="text-center mt-3">
+                <h2>NPO Core</h2>
+                <p>Your one-stop shop for all things nonprofits</p>
+              </div>
+            </a>
+            <a className="product-link" href="http://anojs.com" target="_blank">
+              <img className="product-image" src="img/products/anojs/desktop.png" alt="Launch Tech LLC - Ano.js - Website Image" />
+              <div className="text-center mt-3">
+                <h2>Ano.js</h2>
+                <p>Web Animation made Easy</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </section>
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+      <section className="contact-us" id="contact-us">
+        <div className="section-container container">
+          <h1 className="section-header">Contact Us!</h1>
+          <div className="contact-us-grid">
+            <div className="contact-us-grid-left" data-aos="flip-left">
+              <form action="https://formspree.io/hello@launchtechllc.com" method="post">
+                <label>Name:</label>
+                <input className="ghost-input" type="text" name="Name" placeholder="Name" />
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+                <label>Email:</label>
+                <input className="ghost-input" type="email" name="Email" placeholder="Email" />
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+                <label>Message:</label>
+                <textarea className="ghost-input" name="Message" rows="5" placeholder="Message"></textarea>
+                <input type="submit" className="button solid-cta-button-dark w-100" value="Contact Us!" />
+              </form>
+            </div>
+            <div className="contact-us-grid-right" data-aos="flip-right">
+              <h4>We're completely open to:</h4>
+              <ul className="ml-5">
+                <li><strong>Product Partnerships</strong></li>
+                <li>Community/Corporate partnerships</li>
+                <li>Collaborating on events</li>
+                <li>Product ideas/requests</li>
+                <li>Inquiries and questions</li>
+              </ul>
+              <p>
+                <strong>We promise to respond within 24-48 hours after you email us!</strong>
+              </p>
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+              <div className="ml-4">
+                <p className="contact-us-grid-left-contact-item">
+                  <a href="mailto:hello@launchtechllc.com" target="_blank">
+                    <img src="img/icons/contact/email-dark.svg" />
+                    <span>hello@launchtechllc.com</span>
+                  </a>
+                </p>
+                <p className="contact-us-grid-left-contact-item">
+                  <a href="https://calendly.com/launch-tech" target="_blank">
+                    <img src="img/icons/contact/calendar-dark.svg" />
+                    <span>Schedule a Call</span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      <div className="waves">
+        <img src="img/waves/header-wave.png" className="wave" />
+      </div>
+    </>
   )
 }
