6a5626c6bd05506f65510e76fd6ea96aff0e194e
Calix Huang <calix.huang1@gmail.com>
2020-06-19 23:18:06 -0700

    added partners page

---
+++ pages/partners.js
@@ -0,0 +1,164 @@
+import Layout from '../components/Layout.js'
+
+export default () => {
+  return (
+    <Layout>
+      <div className="waves">
+        <div className="waves-container">
+          <div className="container">
+            <h1 className="section-header">Our Partners</h1>
+          </div>
+        </div>
+        <img src="img/waves/nav-wave.png" className="flipped-wave wave" />
+      </div>
+
+      <section className="our-partners">
+        <div className="page-container container">
+          <div className="partners-grid">
+            <a className="partner" href="https://bluestampengineering.com/" target="_blank">
+              <img src="img/logos/bluestampengineering.png" />
+            </a>
+            <a className="partner" href="https://www.leangap.org" target="_blank">
+              <img src="img/logos/leangap.png" />
+            </a>
+            <a className="partner" href="https://linktr.ee/fmn20" target="_blank">
+              <img src="img/logos/future-minds.png" />
+            </a>
+            <a className="partner" href="https://simplyneuroscience.org/" target="_blank">
+              <img src="img/logos/simplyneuroscience.png" />
+            </a>
+            <a className="partner" href="http://techtreeroot.org/" target="_blank">
+              <img src="img/logos/techtreeroot.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/youngminds.global/" target="_blank">
+              <img src="img/logos/ymg.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/norcalhacks/" target="_blank">
+              <img src="img/logos/norcalhacks.png" />
+            </a>
+            <a className="partner" href="https://planethacks.tech/" target="_blank">
+              <img src="img/logos/planethacks.png" />
+            </a>
+            <a className="partner p-3" href="https://cloud.hackthefog.com/" target="_blank">
+              <img src="img/logos/hackthefog.png" />
+            </a>
+            <a className="partner" href="https://www.healthhacks.tech/" target="_blank">
+              <img src="img/logos/healthhacks.png" />
+            </a>
+            <a className="partner" href="https://dvhacks.tech/" target="_blank">
+              <img src="img/logos/dvhacks.png" />
+            </a>
+            <a className="partner" href="https://atlashacks.tech/" target="_blank">
+              <img src="img/logos/atlashacks.png" />
+            </a>
+            <a className="partner p-4" href="https://xdhacks.com/" target="_blank">
+              <img src="img/logos/xdhacks.png" />
+            </a>
+            <a className="partner p-3" href="https://www.bithacks.io/" target="_blank">
+              <img src="img/logos/bithacks.png" />
+            </a>
+            <a className="partner p-3" href="https://linktr.ee/bayareahacks" target="_blank">
+              <img src="img/logos/bayareahacks.png" />
+            </a>
+            <a className="partner p-2" href="https://hackthenortheast.com/" target="_blank">
+              <img src="img/logos/hackthenortheast.svg" />
+            </a>
+            <a className="partner" href="https://sigmahacks.org/" target="_blank">
+              <img src="img/logos/sigmahacks.png" />
+            </a>
+            <a className="partner" href="https://plathacks.github.io/" target="_blank">
+              <img src="img/logos/plathacks.png" />
+            </a>
+            <a className="partner" href="https://www.thehackventure.com/" target="_blank">
+              <img src="img/logos/thehackventure.png" />
+            </a>
+            <a className="partner" href="https://cryptohacks.tech/" target="_blank">
+              <img src="img/logos/trivalley-crypto-hacks.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/intern_net/" target="_blank">
+              <img src="img/logos/intern_net.png" />
+            </a>
+            <a className="partner" href="https://www.irvinelights.org/" target="_blank">
+              <img src="img/logos/irvinelights.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/zenerations/" target="_blank">
+              <img src="img/logos/zenerations.png" />
+            </a>
+            <a className="partner" href="https://plannrconsulting.wixsite.com/plannrconsulting" target="_blank">
+              <img src="img/logos/plannrconsulting.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/girlsstemit/" target="_blank">
+              <img src="img/logos/girls_stem_it.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/citizensenlightened/" target="_blank">
+              <img src="img/logos/citizens_enlightened.png" />
+            </a>
+            <a className="partner" href="https://www.vitalityvolunteers.com/" target="_blank">
+              <img src="img/logos/vitality_volunteers.png" />
+            </a>
+            <a className="partner" href="https://linktr.ee/medicalwonders" target="_blank">
+              <img src="img/logos/medicalwonders.png" />
+            </a>
+            <a className="partner" href="https://heartofreview.wixsite.com/home" target="_blank">
+              <img src="img/logos/heartofreview.png" />
+            </a>
+            <a className="partner" href="https://proximityfoundation.weebly.com/" target="_blank">
+              <img src="img/logos/proximity.png" />
+            </a>
+            <a className="partner" href="https://www.united2care.org/" target="_blank">
+              <img src="img/logos/united2care.png" />
+            </a>
+            <a className="partner" href="https://linktr.ee/theaffairmag" target="_blank">
+              <img src="img/logos/theaffair.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/allgirlithm/" target="_blank">
+              <img src="img/logos/allgirlithm.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/stemdays/" target="_blank">
+              <img src="img/logos/stemdays.png" />
+            </a>
+            <a className="partner" href="https://tinyurl.com/sampofficial" target="_blank">
+              <img src="img/logos/sampofficial.png" />
+            </a>
+            <a className="partner" href="https://www.instagram.com/feminists4equality/" target="_blank">
+              <img src="img/logos/femminists4equality.png" />
+            </a>
+            <a className="partner p-2" href="https://linktr.ee/melodiesformath" target="_blank">
+              <img src="img/logos/melodiesformath.png" />
+            </a>
+            <a className="partner" href="https://motivatetoeducatet.wixsite.com/site" target="_blank">
+              <img src="img/logos/motivatetoeducate.png" />
+            </a>
+            <a className="partner" href="https://ycwalameda.weebly.com/" target="_blank">
+              <img src="img/logos/ycw.png" />
+            </a>
+            <a className="partner p-3" href="https://www.instagram.com/stemupodcast/" target="_blank">
+              <img src="img/logos/from_the_stem_up.png" />
+            </a>
+            <a className="partner" href="https://helloo-world.com/" target="_blank">
+              <img src="img/logos/helloo.png" />
+            </a>
+            <a className="partner p-3" href="https://www.moderndivergence.com/" target="_blank">
+              <img src="img/logos/moderndivergence.png" />
+            </a>
+            <a className="partner p-3" href="https://www.instagram.com/techshare/" target="_blank">
+              <img src="img/logos/techshare.png" />
+            </a>
+            <a className="partner p-3" href="https://www.instagram.com/girltechboss/" target="_blank">
+              <img src="img/logos/girltechboss.png" />
+            </a>
+            <a className="partner p-3" href="https://linktr.ee/findingyourwhy" target="_blank">
+              <img src="img/logos/finding-your-why.png" />
+            </a>
+            <a className="partner p-3" href="https://www.instagram.com/innotech_smartkids/" target="_blank">
+              <img src="img/logos/innotech.png" />
+            </a>
+            <a className="partner p-3" href="https://linktr.ee/hk_youth_opps" target="_blank">
+              <img src="img/logos/youth-opps.png" />
+            </a>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
