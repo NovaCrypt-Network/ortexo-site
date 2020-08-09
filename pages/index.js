4b3b04dd8da5ccdd86b44082d9cec5f8c0e233da
Michael Wang <michaelwwang17@gmail.com>
2020-08-08 23:15:36 -0400

    changed tagline, meta tag and text under software is changing the world

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import Head from 'next/head'
-import Layout from '../components/Layout.js'
-import Link from 'next/link'
+import Head from "next/head";
+import Layout from "../components/Layout.js";
+import Link from "next/link";
 
 export default () => {
   return (
     <Layout>
       <Head>
         <title>Launch Tech LLC - A SaaS product holding company</title>
-        <meta name="description" content="Launch Tech is a SaaS (Software as a Service) product holding company, created for launching real-world software applications. We create products that are useful, meaningful, and applicable to different people and different situations around the world. We try to build, market, and deploy software through partnerships, events, and sponsors." />
+        <meta
+          name="description"
+          content="Accelerating student success by providing access to new knowledge, insights and opportunities."
+        />
 
         <link rel="stylesheet" href="/css/odometer.css" />
         <script src="/js/odometer.js" />
@@ -18,21 +21,33 @@ export default () => {
         <div className="container">
           <div className="header-grid">
             <div className="header-grid-left">
-              <h1 style={{ "fontSize": "60px", "fontWeight": "bold" }}>Launch Tech LLC</h1>
+              <h1 style={{ fontSize: "60px", fontWeight: "bold" }}>
+                Launch Tech LLC
+              </h1>
               <h4 className="header-grid-left-sub-header">
-                A SaaS product accelerator, dedicated to launching real-world software applications!
+                Providing opportunities and resources for success
               </h4>
               <div className="cta-buttons mt-4">
-                <button onClick={() => {
-                  document.getElementById('about').scrollIntoView({behavior: 'smooth'});
-                }} className="button ghost-cta-button-light">Learn more</button>
+                <button
+                  onClick={() => {
+                    document
+                      .getElementById("about")
+                      .scrollIntoView({ behavior: "smooth" });
+                  }}
+                  className="button ghost-cta-button-light"
+                >
+                  Learn more
+                </button>
                 <Link href="/products">
                   <a className="button solid-cta-button-light">Our products!</a>
                 </Link>
               </div>
             </div>
             <div className="header-grid-right">
-              <img src="img/illustrations/header.svg" className="header-grid-right-illustration" />
+              <img
+                src="img/illustrations/header.svg"
+                className="header-grid-right-illustration"
+              />
             </div>
           </div>
         </div>
@@ -49,8 +64,17 @@ export default () => {
             <img className="px-3" src="/img/logos/trusted-by/tks.png" alt="" />
             <img className="" src="/img/logos/trusted-by/launchx.png" alt="" />
             <img className="px-3" src="/img/logos/leangap.png" alt="" />
-            <img className="px-3" src="/img/logos/bluestampengineering.png" alt="" />
-            <img id="mobile-disappear" className="px-3" src="/img/logos/future-minds.png" alt="" />
+            <img
+              className="px-3"
+              src="/img/logos/bluestampengineering.png"
+              alt=""
+            />
+            <img
+              id="mobile-disappear"
+              className="px-3"
+              src="/img/logos/future-minds.png"
+              alt=""
+            />
           </div>
         </div>
       </section>
@@ -60,21 +84,37 @@ export default () => {
           <div className="about-container">
             <h2>Software is changing the world.</h2>
             <p>
-              That's why we aim to build, improve, and accelerate the growth of products that are applicable to different people and different situations around the world. We reach anyone and everyone with an internet connection, and continue to strive for quality user traction and satisfaction!
+              Launch Tech accelerates success by exposing students to new
+              knowledge, opportunities, and innovative software technologies.
             </p>
           </div>
           <div className="statistics-grid">
             <div className="statistic">
               Total interactions
-              <span className="number"><span class="odometer" id="interactions_odometer">0</span>+</span>
+              <span className="number">
+                <span class="odometer" id="interactions_odometer">
+                  0
+                </span>
+                +
+              </span>
             </div>
             <div className="statistic">
               Happy users
-              <span className="number"><span class="odometer" id="users_odometer">0</span>+</span>
+              <span className="number">
+                <span class="odometer" id="users_odometer">
+                  0
+                </span>
+                +
+              </span>
             </div>
             <div className="statistic">
               Partners
-              <span className="number"><span class="odometer" id="partners_odometer">0</span>+</span>
+              <span className="number">
+                <span class="odometer" id="partners_odometer">
+                  0
+                </span>
+                +
+              </span>
             </div>
           </div>
         </div>
@@ -85,22 +125,46 @@ export default () => {
           <div className="container">
             <h1 className="section-header">Featured Products</h1>
             <div className="products-grid mt-5">
-              <a className="product-link" href="https://www.w3hacks.com" target="_blank">
-                <img className="product-image" src="img/products/w3hacks/desktop.png" alt="Launch Tech LLC - w3Hacks - Website Image" />
+              <a
+                className="product-link"
+                href="https://www.w3hacks.com"
+                target="_blank"
+              >
+                <img
+                  className="product-image"
+                  src="img/products/w3hacks/desktop.png"
+                  alt="Launch Tech LLC - w3Hacks - Website Image"
+                />
                 <div className="text-center mt-3">
                   <h2>w3Hacks</h2>
                   <p>Learn by doing</p>
                 </div>
               </a>
-              <a className="product-link" href="https://npocore.com" target="_blank">
-                <img className="product-image" src="img/products/npocore/desktop.png" alt="Launch Tech LLC - NPO Core - Website Image" />
+              <a
+                className="product-link"
+                href="https://npocore.com"
+                target="_blank"
+              >
+                <img
+                  className="product-image"
+                  src="img/products/npocore/desktop.png"
+                  alt="Launch Tech LLC - NPO Core - Website Image"
+                />
                 <div className="text-center mt-3">
                   <h2>NPO Core</h2>
                   <p>Your one-stop shop for all things non-profits</p>
                 </div>
               </a>
-              <a className="product-link" href="https://anojs.com" target="_blank">
-                <img className="product-image" src="img/products/anojs/desktop.png" alt="Launch Tech LLC - Ano.js - Website Image" />
+              <a
+                className="product-link"
+                href="https://anojs.com"
+                target="_blank"
+              >
+                <img
+                  className="product-image"
+                  src="img/products/anojs/desktop.png"
+                  alt="Launch Tech LLC - Ano.js - Website Image"
+                />
                 <div className="text-center mt-3">
                   <h2>Ano.js</h2>
                   <p>Web Animation made easy</p>
@@ -117,16 +181,38 @@ export default () => {
             <h1 className="section-header">Contact Us!</h1>
             <div className="contact-us-grid">
               <div className="contact-us-grid-left">
-                <form action="https://formspree.io/hello@launchtechllc.com" method="post">
+                <form
+                  action="https://formspree.io/hello@launchtechllc.com"
+                  method="post"
+                >
                   <label>Name:</label>
-                  <input className="ghost-input" type="text" name="Name" placeholder="Name" />
+                  <input
+                    className="ghost-input"
+                    type="text"
+                    name="Name"
+                    placeholder="Name"
+                  />
 
                   <label>Email:</label>
-                  <input className="ghost-input" type="email" name="Email" placeholder="Email" />
+                  <input
+                    className="ghost-input"
+                    type="email"
+                    name="Email"
+                    placeholder="Email"
+                  />
 
                   <label>Message:</label>
-                  <textarea className="ghost-input" name="Message" rows="5" placeholder="Message"></textarea>
-                  <input type="submit" className="button solid-cta-button-dark w-100" value="Contact Us!" />
+                  <textarea
+                    className="ghost-input"
+                    name="Message"
+                    rows="5"
+                    placeholder="Message"
+                  ></textarea>
+                  <input
+                    type="submit"
+                    className="button solid-cta-button-dark w-100"
+                    value="Contact Us!"
+                  />
                 </form>
               </div>
               <div className="contact-us-grid-right">
@@ -138,7 +224,9 @@ export default () => {
                   <li>Inquiries and questions</li>
                 </ul>
                 <p>
-                  <strong>We promise to respond within 24-48 hours after you email us!</strong>
+                  <strong>
+                    We promise to respond within 24-48 hours after you email us!
+                  </strong>
                 </p>
 
                 <div className="ml-4">
@@ -160,7 +248,6 @@ export default () => {
           </div>
         </div>
       </section>
-
     </Layout>
-  )
-}
+  );
+};
