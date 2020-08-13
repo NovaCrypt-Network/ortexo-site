f6a84784523e9896da6a6e02ffec6ee83eeff941
Mitchell Artz <32065190+MitchellArtz@users.noreply.github.com>
2020-08-12 20:51:43 -0400

    Removed Molly and Rex

--- pages/about.js
+++ pages/about.js
@@ -186,7 +186,7 @@ export default () => {
                     </a>
                   </div>
                 </div>
-                <p className="person-title">Director of Testing</p>
+                <p className="person-title">Director of Software Development</p>
                 <small className="person-description">
                   Mitchell Artz is a QA tester and security enthusiast,
                   currently studying to get his High School diploma online. He
@@ -278,71 +278,6 @@ export default () => {
                   students, and hopes to further her own programming knowledge.
                   In her free time, Angela enjoys running, hurdling and
                   listening to music.
-                </small>
-              </div>
-            </div>
-            <div className="person" data-aos="zoom-in">
-              <div className="image-cropper">
-                <img
-                  src="img/people/rex.png"
-                  alt="Ortexo - Rex Liu"
-                  className="person-image"
-                />
-              </div>
-              <div className="person-content">
-                <div className="person-info">
-                  <div>
-                    <h3 className="person-name">Rex Liu</h3>
-                  </div>
-                  <div className="person-info-links">
-                    <div></div>
-                    <a
-                      href="https://www.linkedin.com/in/rexliu3/"
-                      target="_blank"
-                    >
-                      <img src="img/icons/social-media/linkedin-dark.svg" />
-                    </a>
-                    <div></div>
-                  </div>
-                </div>
-                <p className="person-title">QA Tester</p>
-                <small className="person-description">
-                  Rex Liu is an independent software engineer developing web and
-                  mobile applications for clients and creating personal projects
-                  to dive deeper into the world of CS. He is also an avid
-                  badminton and tennis player.
-                </small>
-              </div>
-            </div>
-            <div className="person" data-aos="zoom-in">
-              <div className="image-cropper">
-                <img
-                  src="img/people/molly.png"
-                  alt="Ortexo - Molly Xu"
-                  className="person-image"
-                />
-              </div>
-              <div className="person-content">
-                <div className="person-info">
-                  <div>
-                    <h3 className="person-name">Molly Xu</h3>
-                  </div>
-                  <div className="person-info-links">
-                    <div></div>
-                    <a
-                      href="https://www.linkedin.com/in/mollyxu9/"
-                      target="_blank"
-                    >
-                      <img src="img/icons/social-media/linkedin-dark.svg" />
-                    </a>
-                  </div>
-                </div>
-                <p className="person-title">QA Tester</p>
-                <small className="person-description">
-                  Molly is a prospective Computer Science and Business
-                  Administration double degree student at the University of
-                  Waterloo. When she isn’t coding, you can find her listening to
-                  Latin pop or binge-watching La Casa de Papel (Money Heist).
                 </small>
               </div>
             </div>
